473c49b6380d3b4d76e8e53d09d20f73456f4ec1
abdellah chotta <abdellahchotta55@gmail.com>
2023-03-08 15:02:57 +0100

    1

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,34 +1,58 @@
-import React, { useState,useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { FiSearch } from "react-icons/fi";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import SongCard from "../components/SongCard";
+import axios from "axios";
+function Search() {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
+  const { searchTerm } = useParams();
+  const [data, setData] = useState();
 
+  useEffect(() => {
+    const fetchData = async (term) => {
+      const headers = {
+        "X-RapidAPI-Key": "9162fd2d6cmshc039b9bda0d6bb4p159f01jsn4d136b001cb2",
+        "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+      };
+      const params = {
+        term: searchTerm,
+        locale: "en-Us",
+        offset: "0",
+        limit: "5",
+      };
 
-export default function Searchbar() {
-  const navigate = useNavigate();
-  const [searchTerm, setsearchTerm] = useState("");
+      const response = await axios.get("https://shazam.p.rapidapi.com/search", {
+        headers,
+        params,
+      });
+      setData(response.data.tracks.hits);
+    };
+    fetchData();
+  }, [searchTerm]);
+  if (!data) {
+    return <>....</>;
+  } else
+    return (
+      <div className="flex flex-col">
+        <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+          Showing results for <span className="font-black">{searchTerm}</span>
+        </h2>
 
-  const handeleSubmit = (e) => {
-    e.preventDefault();
-    navigate(`/search/${searchTerm}`);
-  };
-
-  return (
-    <form className="form" onSubmit={handeleSubmit}>
-      <label htmlFor="search-field">Search all files</label>
-      <div className="search_container">
-        <FiSearch className="search_icon" />
-        <input
-          type="search"
-          name="search-field"
-          autoComplete="off"
-          id="search-field"
-          className="search_field"
-          placeholder="Search"
-          value={searchTerm}
-          onChange={(e) => setsearchTerm(e.target.value)}
-        />
+        <div className="songs_container">
+          {data.map((song, i) => (
+            <SongCard
+              key={song.track.key}
+              song={song.track}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              data={data}
+              i={i}
+            />
+          ))}
+        </div>
       </div>
-    </form>
-  );
+    );
 }
+
+export default Search;
